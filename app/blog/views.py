7346e40add98d11f46544be8523ad6c727b6effb
Boris <borislianggz@gmail.com>
2017-05-08 23:08:23 +0800

    Add Welcome massage and apply material design.

--- app/blog/views.py
+++ app/blog/views.py
@@ -3,4 +3,16 @@ from flask import render_template
 
 @blog.route('/')
 def home_page():
-    return render_template("base.html")
+    return render_template("index.html")
+
+@blog.route('/about')
+def about_me():
+    return render_template("about.html")
+
+@blog.app_errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+@blog.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"),500
